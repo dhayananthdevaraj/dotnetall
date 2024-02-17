c8306ac64e75ffac4addf461f82121d0bbb1a5f4
neojarvis <techadmin@iamneo.ai>
2024-02-17 02:56:35 +0000

    Auto-save-commit-17/02/2024--08:26:32

--- cms_update/dotnetapp/Services/AuthService.cs
+++ cms_update/dotnetapp/Services/AuthService.cs
@@ -68,6 +68,7 @@ namespace dotnetapp.Services
                new Claim(ClaimTypes.Name, user.UserName),
                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim("userId", user.Id.ToString()), 
             };
 
             foreach (var userRole in userRoles)
