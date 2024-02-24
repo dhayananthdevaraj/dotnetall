0ed16f3bc1196c85f9872c399cfef4a8179ded35
neojarvis <techadmin@iamneo.ai>
2024-02-24 13:01:21 +0000

    Auto-save-commit-24/02/2024--18:31:19

--- studenten/dotnetapp/Models/User.cs
+++ studenten/dotnetapp/Models/User.cs
@@ -5,18 +5,22 @@
     {
         public class User
         {
-            public long UserId { get; set; }
+
             public string Email { get; set; }
-            public string Password { get; set; }
-            public string Username { get; set; }
-            public string MobileNumber { get; set; }
-            public string UserRole { get; set; }
+        public long UserId { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string MobileNumber { get; set; }
+        public string UserRole { get; set; }
 
-            [JsonIgnore]
-            public List<Assignment>? Assignments { get; set; }
+        // Navigation property for Student
+        public Student? Student { get; set; }
 
-            [JsonIgnore]
-            public List<Issue>? Issues { get; set; }
+        // Navigation property for Courses
+        public ICollection<Course>? Courses { get; set; }
 
         }
     }
+
+
+
